b2c6f517133a0cf6d8cfdc916138fa036fb63f1b
Rob Speed <speed.rob@gmail.com>
2010-12-24 18:42:36 -0500

    Made get_hostname a public function. Improved documentation. Removed a bunch of unnecessary functions. Nothing was planned, really. Removed the necessity to pass locals() by simply returning the settings. Merge the settings dicts inline with the return (perhaps not as Pythonic, but it benchmarks the same and seems more readable).

--- __init__.py
+++ __init__.py
@@ -6,10 +6,10 @@ http://creativecommons.org/licenses/MIT/
 """
 import os, re
 
-def _get_hostname():
+def get_hostname():
 	hostname = os.uname()[1]
 
-	# Convert the machine's hostname into alphanumeric characters
+	# Convert the machine's hostname into alphanumeric characters, suitable for python module names
 	# Note: We don't want to use \W because it also matches underscores.
 	#       The string segment "._." would end up as "___" instead of "_".
 	hostname = re.sub('[^a-zA-Z0-9]+', '_', hostname)
@@ -17,15 +17,7 @@ def _get_hostname():
 	return hostname
 
 
-def import_via_hostname(parent_locals):
-	import_settings(_get_hostname(), parent_locals)
-
-
-def import_via_hostname_dict(hostnames):
-	import_settings(hostnames.get(_get_hostname()), parent_locals)
-
-
-def import_settings(module, parent_locals):
+def import_settings(module):
 	# If the requested settings file isn't found, default to the production settings
 	try:
 		_get_settings(module)
@@ -34,8 +26,7 @@ def import_settings(module, parent_locals):
 		module = 'production'
 
 	# Load the config settings properties into the local scope
-	for key, value in _climb_settings(module).items():
-		parent_locals[key] = value
+	return _climb_settings(module)
 
 
 def _climb_settings(name):
@@ -47,9 +38,7 @@ def _climb_settings(name):
 		return settings
 	else:
 		del settings['EXTENDS']
-		parent_settings = import_settings(parent_module_name)
-		parent_settings.update(settings)
-		return parent_settings
+		return dict(_get_settings(parent_module_name), **settings)
 
 
 def _get_settings(name):
