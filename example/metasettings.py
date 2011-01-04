a47a43bf2eb77b2538c3ad05a37675e23706cc2f
Rob Speed <speed.rob@gmail.com>
2011-01-04 06:40:30 -0500

    Added the ability to continue adding modules (for example: if you specify different modules based on different parameters). Updated the example project accordingly.

--- example/metasettings.py
+++ example/metasettings.py
@@ -1,9 +1,10 @@
 import meta_settings
 from django.conf import settings
 
-patterns = meta_settings.patterns(
+patterns = meta_settings.patterns('settings')
+
+patterns.add_modules(
 	meta_settings.HOSTNAME,
-	'settings',
 	(r'production1', 'production'),
 	(r'stage1', ('production', 'stage')),
 	(r'dev1', ('development')),
@@ -12,4 +13,4 @@ patterns = meta_settings.patterns(
 )
 
 # Pull the merged settings and pass them to settings.configure
-settings.configure( **patterns.settings() )
+settings.configure( **patterns.get_settings() )
--- meta_settings/__init__.py
+++ meta_settings/__init__.py
@@ -12,38 +12,52 @@ FQDN = 2
 PATH = 3
 
 class patterns():
-	def __init__(self, method, settings_dir, *patterns):
+	_import_complete = lambda self: len(self._settings.keys()) > 0
+
+	def __init__(self, settings_dir):
 		self._settings_dir = settings_dir
 		self._settings = {}
+		self._modules = []
+
+
+	def add_modules(self, method, *patterns):
+		if self._import_complete():
+			raise RuntimeError('Settings already configured.')
 
 		match = {
 			HOSTNAME: lambda: socket.gethostname(),
-			FQDN: lambda: None,
+			FQDN: lambda: socket.getfqdn(),
 			PATH: lambda: None,
 			None: lambda: None,
 		}.get(method)()
 
 		for pattern in patterns:
 			if bool(re.match(pattern[0], match)):
-				self._modules = pattern[1]
+				modules = pattern[1]
 
 				# Make sure the list of modules is either a list or a tuple
-				if type(self._modules) not in (list, tuple):
-					self._modules = (self._modules,)
+				if type(modules) not in (list, tuple):
+					modules = (modules,)
+
+				# Add the modules to the module list
+				self._modules += modules
 
 				return
 
 		raise Exception("No match for %s." % match)
 
+
 	def import_settings(self, name):
 			execfile('%s/%s.py' % (self._settings_dir, name), globals(), self._settings)
 
 
-	def settings(self):
-		self.import_settings('base')
+	def get_settings(self):
+		# Only import the modules once
+		if not self._import_complete():
+			self.import_settings('base')
 
-		for module in self._modules:
-			self.import_settings(module)
+			for module in self._modules:
+				self.import_settings(module)
 
 		# Only return settings with upper-case names
 		return dict([(setting, self._settings[setting]) for setting in self._settings.keys() if setting.isupper()])
