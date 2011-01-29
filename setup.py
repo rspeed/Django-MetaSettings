45eccf72c2d1368a329e9c5546984ff36883f2a6
Rob Speed <speed.rob@gmail.com>
2011-01-29 11:05:17 -0500

    Compartmentalized the process that filters out settings without upper-case names. Added documentation to the import_settings method.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django-MetaSettings',
+	version='0.1',
+	description='',
+	author='Rob Speed',
+	author_email='speed.rob@gmail.com',
+	url='https://github.com/rspeed/Django-MetaSettings',
+	packages=['metasettings'],
+)
