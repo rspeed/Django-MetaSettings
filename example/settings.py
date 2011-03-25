966bce0721c7b6bd1667a347a55d4eeae7c74574
Rob Speed <speed.rob@gmail.com>
2011-03-25 07:27:35 -0400

    Moved base configuration to settings.py for the example project.

--- example/settings.py
+++ example/settings.py
@@ -11,5 +11,63 @@ METASETTINGS_PATTERNS = (
 	(r'dev1', ('development')),
 
 	(r'glados', ('development', 'glados')),
-	(r'server.8207std.private', ('base', 'development', 'glados'))
 )
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+	('Project Lead', 'project_lead@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': '/var/www/example.com/db/default.sqlite',
+	}
+}
+
+TIME_ZONE = 'America/New_York'
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+ADMIN_MEDIA_PREFIX = '/media/'
+
+SECRET_KEY = 'v%eryyk0u!l^tsu0qzqoed@cucs!*3htobh$o2jf5ytz)a^2d^'
+
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.Loader',
+	'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+	'django.middleware.common.CommonMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+)
+
+INSTALLED_APPS = (
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.admin',
+)
+
+MEDIA_ROOT = '/var/www/example.com/media'
