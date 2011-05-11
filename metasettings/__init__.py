a43b92ce798d443c89a56c2005f937ca018c270d
Rob Speed <speed.rob@gmail.com>
2011-05-11 01:43:44 -0400

    Removed PATH matching. It never had any code to actually determine the value and I couldn't come up with a way to use it. Added VAR matching. Used to match based on a variable in settings.py. Since it defaults to "PROJECT_PATH", it effectively replaces PATH matching. Added settings for the names of environmental and settings variables. This replaces the ENV_NAME global used before. Added comments describing the variables as they're loaded. Added a comment describing what the big scary "match" dict does.

--- metasettings/__init__.py
+++ metasettings/__init__.py
@@ -15,23 +15,34 @@ from django.utils import importlib
 
 HOSTNAME = 'METASETTINGS_MATCHING_METHOD_HOST_NAME'
 FQDN = 'METASETTINGS_MATCHING_METHOD_DOMAIN_NAME'
-PATH = 'METASETTINGS_MATCHING_METHOD_ROOT_PATH'
 ENV = 'METASETTINGS_MATCHING_METHOD_ENVIRONMENTAL_VARIABLE'
-
-# Name of the environment variable containing the key for ENV matching
-ENV_NAME = 'METASETTINGS_KEY'
+VAR = 'METASETTINGS_MATCHING_METHOD_SETTINGS_VAR'
 
 
 def init(_globals):
-	meta_method = _globals.get("METASETTINGS_METHOD", HOSTNAME) # Default to hostname matching
-	meta_dir = _globals.get("METASETTINGS_DIR", 'settings') # Default to the "settings" folder
-	meta_patterns = _globals.get("METASETTINGS_PATTERNS", (None, [])) # Default to something that just doesn't work
+	# The method used for matching
+	meta_method = _globals.get("METASETTINGS_METHOD", HOSTNAME)
 
+	# The directory where settings files are stored
+	meta_dir = _globals.get("METASETTINGS_DIR", 'settings')
+
+	# The list of patterns to match against, defaults to doing nothing
+	meta_patterns = _globals.get("METASETTINGS_PATTERNS", (r'.*', []))
+
+
+	# Name of the environment variable containing the key for ENV matching
+	env_name = _globals.get("METASETTINGS_ENV_NAME", 'METASETTINGS_KEY')
+
+	# Name of the settings variable containing the value for VAR matching
+	path_var_name = _globals.get("METASETTINGS_VAR_NAME", "PROJECT_PATH")
+
+
+	# Determine the matching method and get the value to match against
 	match = {
 		HOSTNAME: lambda: socket.gethostname(),
 		FQDN: lambda: socket.getfqdn(),
-		PATH: lambda: None,
-		ENV: lambda: os.getenv(ENV_NAME),
+		ENV: lambda: os.getenv(env_name),
+		VAR: lambda: _globals.get('PROJECT_PATH'),
 		None: lambda: None,
 	}.get(meta_method)()
 
