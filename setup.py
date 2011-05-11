32d04b80dd13d707e09371f1e4ff0fc29caefe65
Rob Speed <speed.rob@gmail.com>
2011-05-11 00:04:44 -0400

    Bumped the version number to 0.1.1 to reflect the recent changes by twidi.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Django-MetaSettings',
-	version='0.1',
+	version='0.1.1',
 	description='Simplify the obnoxious task of configuring the same Django project in multiple environments.',
 	author='Rob Speed',
 	author_email='speed.rob@gmail.com',
